f99118fd7659e8e9301ea328177fa1b3515e20e4
Mark Tang <markta31@gmail.com>
2017-09-05 17:21:54 +0800

    publish.el: html files are in-place generated, instead of copying to out/ directory.

--- publish.el
+++ publish.el
@@ -1,18 +1,17 @@
-(setq my-blog-project-root (file-name-directory (buffer-file-name)))
-(setq my-blog-output-dir "out")
-(setq my-blog-blog-source-dir (concat my-blog-project-root "/" "blog"))
-(setq my-blog-blog-output-dir (concat my-blog-project-root "/" my-blog-output-dir "/" "blog"))
+;; Usage: execute `M-x eval-buffer' the first time you open Emacs,
+;; later you can execute `M-x org-publish-all' anywhere to re-publish.
 
+(setq my-blog-project-root (file-name-directory (buffer-file-name)))
+(setq my-blog-blog-source-dir (concat my-blog-project-root "/" "blog"))
 
 (setq org-publish-project-alist
       (list (list "blog"
                   ':base-directory my-blog-blog-source-dir
                   ':base-extension "org"
-                  ':publishing-directory my-blog-blog-output-dir
+                  ':publishing-directory my-blog-blog-source-dir
                   ':publishing-function 'org-html-publish-to-html
                   ':headline-levels 3
                   ':section-numbers nil
                   ':with-toc nil)))
 
 (org-publish-all)
-
