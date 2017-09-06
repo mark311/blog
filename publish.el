19af980afffdb9c5fadf2fbb0f93551e27e076de
Mark Tang <markta31@gmail.com>
2017-09-06 20:46:40 +0800

    seperate formal blog from draft blog, and adjust header of some blogs

--- publish.el
+++ publish.el
@@ -30,16 +30,22 @@
 (setq my-blog-project-root ".")
 (setq my-blog-blog-source-dir (concat my-blog-project-root "/" "blog"))
 
+(defun my-blog-make-blog-project (project-name folder)
+  "Make a blog org project entry, which can be append into
+`org-publish-project-alist'. "
+  (list project-name
+        ':base-directory folder
+        ':base-extension "org"
+        ':publishing-directory folder
+        ':publishing-function 'org-html-publish-to-html
+        ':headline-levels 3
+        ':section-numbers nil
+        ':with-toc nil
+        ':html-head "<link rel=\"stylesheet\" href=\"../css/default.css\" type=\"text/css\" />"))
+
 (setq org-publish-project-alist
-      (list (list "blog"
-                  ':base-directory my-blog-blog-source-dir
-                  ':base-extension "org"
-                  ':publishing-directory my-blog-blog-source-dir
-                  ':publishing-function 'org-html-publish-to-html
-                  ':headline-levels 3
-                  ':section-numbers nil
-                  ':with-toc nil
-                  ':html-head "<link rel=\"stylesheet\" href=\"../css/default.css\" type=\"text/css\" />")))
+      (list (my-blog-make-blog-project "blog" "./blog")
+            (my-blog-make-blog-project "draft-blog" "./draft-blog")))
 
 (org-publish-all)
 
--- publish.sh
+++ publish.sh
@@ -4,7 +4,12 @@ cd $(dirname $(pwd)/$0)
 if [ ! -d blog ]; then
     mkdir blog
 fi
-cp -p blog-src/*.org blog/
+cp -p blog.src/*.org blog/
+
+if [ ! -d draft-blog ]; then
+    mkdir draft-blog
+fi
+cp -p draft-blog.src/*.org draft-blog/
 
 emacs --batch --script publish.el
 
