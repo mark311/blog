c0b9b8e6c14c551ca925d501a1f25bef6b2dcb67
Mark Tang <markta31@gmail.com>
2017-09-07 00:05:34 +0800

    add comments system

--- publish.el
+++ publish.el
@@ -30,6 +30,21 @@
 (setq my-blog-project-root ".")
 (setq my-blog-blog-source-dir (concat my-blog-project-root "/" "blog"))
 
+(setq org-html-postamble-format '(("en" "
+<div>
+<br />
+<p class=\"author\">Author: %a</p>
+<p class=\"date\">Date: %d</p>
+<br />
+<br />
+</div>
+
+<!-- UY BEGIN -->
+<div id=\"uyan_frame\"></div>
+<script type=\"text/javascript\" src=\"http://v2.uyan.cc/code/uyan.js\"></script>
+<!-- UY END -->
+")))
+
 (defun my-blog-make-blog-project (project-name folder)
   "Make a blog org project entry, which can be append into
 `org-publish-project-alist'. "
@@ -41,7 +56,8 @@
         ':headline-levels 3
         ':section-numbers nil
         ':with-toc nil
-        ':html-head "<link rel=\"stylesheet\" href=\"../css/default.css\" type=\"text/css\" />"))
+        ':html-head "<link rel=\"stylesheet\" href=\"../css/default.css\" type=\"text/css\" />"
+        ':html-postamble t))
 
 (setq org-publish-project-alist
       (list (my-blog-make-blog-project "blog" "./blog")
